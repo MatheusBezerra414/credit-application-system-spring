15dc5bc007a0e93f4916fab61c08ea14c7ae57ae
Matheus Bezerra <matheusrpbezerra@gmail.com>
2023-10-26 10:28:00 -0300

    Update CustomerController: Add @Patch

--- src/main/kotlin/com/deveus/credit/application/system/controler/CustomerController.kt
+++ src/main/kotlin/com/deveus/credit/application/system/controler/CustomerController.kt
@@ -1,6 +1,7 @@
 package com.deveus.credit.application.system.controler
 
 import com.deveus.credit.application.system.dto.req.CustomerDTO
+import com.deveus.credit.application.system.dto.req.CustomerUpdateDTO
 import com.deveus.credit.application.system.dto.res.CustomerView
 import com.deveus.credit.application.system.model.Customer
 import com.deveus.credit.application.system.service.impl.CustomerService
@@ -10,11 +11,13 @@ import org.springframework.http.HttpStatusCode
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PatchMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 
@@ -31,21 +34,32 @@ class CustomerController(
     }
 
     @GetMapping
-    fun findCustomer(): ResponseEntity<List<CustomerView>>{
-        val listCustomer: List<CustomerView> = this.customerService.findAll().map{ it -> CustomerView(it) }
+    fun findCustomer(): ResponseEntity<List<CustomerView>> {
+        val listCustomer: List<CustomerView> = this.customerService.findAll().map { it -> CustomerView(it) }
         return ResponseEntity.status(HttpStatus.OK).body(listCustomer)
     }
 
     @GetMapping("/{id}")
-    fun findCustomerById(@PathVariable id: Long): ResponseEntity<CustomerView>{
+    fun findCustomerById(@PathVariable id: Long): ResponseEntity<CustomerView> {
         val customerById: Customer = this.customerService.findById(id)
-        val customerViewById =  CustomerView(customerById)
+        val customerViewById = CustomerView(customerById)
         return ResponseEntity.status(HttpStatus.OK).body(customerViewById)
     }
 
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    fun deleteCustomer(@PathVariable id: Long){
+    fun deleteCustomer(@PathVariable id: Long) {
         this.customerService.delete(id)
+    }
+
+    @PatchMapping
+    fun updateCustomer(
+        @RequestParam(value = "customerId") id: Long,
+        @RequestBody customerUpdate: CustomerUpdateDTO
+    ): ResponseEntity<CustomerView> {
+        val customer = customerService.findById(id)
+        val customerToUpdate = customerUpdate.toEntity(customer)
+        val customerUpdated = this.customerService.save(customerToUpdate)
+        return ResponseEntity.status(HttpStatus.OK).body(CustomerView(customerUpdated))
     }
 }
